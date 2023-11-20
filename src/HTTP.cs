c7e38b9dc50f5208b7185efaf7c465b3ccc7943a
Dean Sellas <dean.sellas@delfaxsolutionsinc.onmicrosoft.com>
2023-11-20 16:19:30 -0600

    Add project files.

---
+++ src/HTTP.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeanHelper
+{
+    public class HTTP
+    {
+        public class HTTPHeader
+        {
+            public string Header { get; }
+            public string Value { get; }
+            public HTTPHeader(string header, string value)
+            {
+                Header = header;
+                Value = value;
+            }
+        }
+        public static async Task<HttpResponseMessage> Get(string uri)
+        {
+            return await Get(uri, new List<HTTPHeader>());
+        }
+        /// <summary>
+        /// Helper Function to make HTTP Get Requests
+        /// </summary>
+        /// <param name="uri">URI of HTTP GET Request</param>
+        /// <param name="headerList">HTTP Headers</param>
+        /// <returns>HTTPResponseMessage</returns>
+        public static async Task<HttpResponseMessage> Get(string uri, List<HTTPHeader> headerList)
+        {
+#if DEBUG
+            Console.WriteLine(String.Format("HTTP GET request to: {0}", uri));
+#endif
+            HttpClient httpClient = new HttpClient();
+
+            foreach (HTTPHeader header in headerList)
+            {
+                httpClient.DefaultRequestHeaders.Add(header.Header, header.Value);
+            }
+
+            return await httpClient.GetAsync(new Uri(uri).ToString());
+        }
+    }
+}
---
+++ src/IEnum.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+
+namespace DeanHelper
+{
+    public class IEnum
+    {
+        public static void ToCSV<T>(IEnumerable<T> l, string path = "", string filename = "out.csv")
+        {
+            if (l.Count() == 0)
+            {
+                return;
+            }
+
+            if (path == "" || path == null)
+            {
+                path = Directory.GetCurrentDirectory();
+            }
+            path += string.Format("\\{0}", filename);
+
+            // Create a StringBuilder to build the CSV content
+            StringBuilder csvContent = new StringBuilder();
+
+            // Get property names from the first item in the list
+            var propertyNames = GetPropertyNames(l.First());
+
+            // Append header
+            csvContent.AppendLine(string.Join(",", propertyNames));
+
+            // Append data
+            foreach (var data in l)
+            {
+                // Extract property values using reflection
+                var propertyValues = GetPropertyValues(data, propertyNames);
+                csvContent.AppendLine(string.Join(",", propertyValues));
+            }
+
+            // Write the CSV content to the file
+            File.WriteAllText(path, csvContent.ToString());
+        }
+        static List<string> GetPropertyNames(dynamic obj)
+        {
+            var propertyNames = new List<string>();
+            var type = obj.GetType();
+
+            foreach (var property in type.GetProperties())
+            {
+                propertyNames.Add(property.Name);
+            }
+
+            return propertyNames;
+        }
+
+        static List<string> GetPropertyValues(dynamic obj, List<string> propertyNames)
+        {
+            var propertyValues = new List<string>();
+
+            foreach (var propertyName in propertyNames)
+            {
+                // Use reflection to get the value of each property
+                var propertyValue = obj.GetType().GetProperty(propertyName).GetValue(obj, null);
+                propertyValues.Add(propertyValue.ToString());
+            }
+
+            return propertyValues;
+        }
+    }
+}
